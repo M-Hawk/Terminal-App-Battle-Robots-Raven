c208acdaa96e69d60daca5fdf1fff4243da829f8
M-Hawk <12922@coderacademy.edu.au>
2022-09-21 14:43:42 +1000

    Added body, weapon attributes to Player Class, added getters, setters for both, changed game_mode function to have options menu

--- src/classes.py
+++ src/classes.py
@@ -1,13 +1,16 @@
 from random import choice
 from os import system, name
 from time import sleep
+from simple_term_menu import TerminalMenu
 
 # Player Class for inidividual human players
 
 class Player():
-    def __init__(self, name, health = 200):
+    def __init__(self, name, health = 200, body_movement = "undefined", arm_weapon = "undefined"):
         self.__name = name
         self.__health = health
+        self.__body_movement = body_movement
+        self.__arm_weapon = arm_weapon
 
     def get_name(self):
        return self.__name
@@ -17,6 +20,13 @@ class Player():
 
     def set_name(self, name):
         self.__name = name
+
+    def set_body_movement(self, body_movement):
+        self.__body_movement = body_movement
+
+    def set_arm_weapon(self, arm_weapon):
+        self.__arm_weapon = arm_weapon
+
 
 # Child Class of the Player Class for AI specifics
 
@@ -26,51 +36,56 @@ class ComputerPlayer(Player):
     bot_names = ["Terminator", "Annihilator", "Destroyer", "Crusher", "Nuts-n-Bolts", "Link",
     "Mechanicus", "Ripper", "Tank", "Shredder"]
     # Added private variable name here to prevent potential access to this random name variable
-    __rand_name = choice(bot_names)
 
-    def __init__(self, name = __rand_name):
-        self.__name = name
-        super().__init__(self)
+    def __init__(self):
+        self.__name = choice(ComputerPlayer.bot_names)
+        super().__init__(self.__name)
 
-    # FIND A BETTER WAY THAN REPEATING CODE TO USE GETTERS SETTERS
-    def get_name(self):
-       return self.__name
 
-    def get_health(self):
-       return self.__health
 
-    def set_name(self, name):
-        self.__name = name
 
 
 class Game():
-
-    def __init__(self):
+    def __init__(self, player_one, player_two):
+        # self.p2 = ComputerPlayer()
         ""
-
-    def clear_terminal(self):
+    # Method that clears the terminal throughout the game so players don't have to scroll
+    def clear_terminal():
         system("cls" if name == "nt" else "clear")
 
+    # Game mode select method
     def game_mode(self):
+        Game.clear_terminal()
         options = ["Single Player", "Multi Player", "Exit Game"]
-        for index, item in enumerate(options):
-            print(f"\n{index+1}) {item}\n")
-        self.game_type = input("What game mode would you like to play? ")
-        self.game_type.capitilize()
-        if self.gametype == 
+        terminal_menu = TerminalMenu(options)
+        menu_entry_index = terminal_menu.show()
+        print(f"You have selected {options[menu_entry_index]}!")
+        while menu_entry_index != 2:
+            if options[menu_entry_index]
+        # if self.game = single_player_mode()
+        # self.game = multi_player_mode()
+
+    def single_player_mode(self):
+        pass
 
 
+    def multi_player_mode(self):
+        pass
 
-
+    # def 
 
 
 # MAINNNN HERE
 
-game = Game()
-game.game_mode()
 
-# player1 = Player("Jax")
-# bot1 = ComputerPlayer()
+
+player_one = Player("Jax")
+player_two = ComputerPlayer()
 
 # print(player1.get_name())
 # print(bot1.get_name())
+# print(bot1.get_health())
+
+
+game = Game(player_one, player_two)
+game.game_mode()
