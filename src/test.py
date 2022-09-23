eeec4f4ab1fba9fc6b64c53e9c995870739e7e1e
M-Hawk <12922@coderacademy.edu.au>
2022-09-23 22:57:29 +1000

    added some code to manipulate into test cases

--- src/test.py
+++ src/test.py
@@ -1,5 +1,20 @@
-options = ["health", "first", "tent", "big"]
 
-options += ["works"]
+# unit tests
+import unittest
+import classes
 
-print(options)
+class TestStringMethods(unittest.TestCase):
+    # test function to test equality of two value
+    def test_negative(self):
+        firstValue = "geeks"
+        secondValue = "gfg"
+        # error message in case if test case got failed
+        message = "First value and second value are not equal !"
+        # assertEqual() to check equality of first & second value
+        self.assertEqual(firstValue, secondValue, message)
+
+
+assert_equals(calculate_damage("fire", "water", 100, 100), 25)
+
+if __name__ == '__main__':
+    unittest.main()
