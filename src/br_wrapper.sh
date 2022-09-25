171c821e4dfcfb5ed54840dd10c0e864a54d664b
Matthew Hawkins <95516628+M-Hawk@users.noreply.github.com>
2022-09-25 18:20:45 +1000

    Delete br_wrapper.sh

--- src/br_wrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-if [[-x '$(command -v python3)']]
-then
-    # any code we put here will run only when the condition is True
-else
-    # optional code to execute if some_condition is False
-fi
